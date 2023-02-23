158b56a8c23008ad4cfeb61f966c6b5806e6e295
Brian Phelan <brianphelan59@gmail.com>
2023-02-23 00:14:15 -0500

    Added a readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Center } from '@chakra-ui/react';
+import { Center, Text, Flex } from '@chakra-ui/react';
 import Leaderboard from './Leaderboard/Leaderboard';
 import { useState, useEffect, useCallback } from 'react';
 import data from './Leaderboard/data.json'
@@ -9,11 +9,23 @@ function App(  ) {
     <>
     {data &&
       <>
-      <Center>
-        <Leaderboard
-            initialData={data}
-          />
-      </Center>
+        <Flex flexDir={'row'} justify='space-evenly'>
+          <Text mt='auto' mb='auto' maxW='350px' justify='center'>
+            <ul>
+              <li>
+                  This purpose of this leaderboard is to give recognition and visiblity to salespeople 
+              </li>
+              <li>
+               <a href='https://www.npmjs.com/package/framer-motion'>framer-motion</a> and <a href='https://www.npmjs.com/package/react-countup'>react-countup</a> were used
+              to animate the leaderboard after a new sale was made
+              </li>
+              <li>This demo increments a random persons data every 5 seconds to simulate a sale</li>
+            </ul>
+          </Text>
+          <Leaderboard
+              initialData={data}
+            />
+        </Flex>
       </>
     }
     </>
