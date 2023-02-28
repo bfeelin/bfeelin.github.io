4369223213b99886461a7e10017b19cedf11e7c4
Brian Phelan <brianphelan59@gmail.com>
2023-02-28 12:39:18 -0500

    Removed props

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
-import data from './Leaderboard/data.json'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
-      <App jsonData={data}/>
+      <App/>
     </ChakraProvider>
 );
